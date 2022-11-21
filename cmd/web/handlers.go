4cd6e1cfecaac3ebff148d0b9c2d2b1bf59d6cad
Darwin Perdomo <ZeroBl@hotmail.com>
2022-11-21 00:29:50 -0400

    feat(cmd/web): Add html templates

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,14 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
+	"text/template"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
+	}
+  
+  files := []string{
+    "./ui/html/base.tmpl",
+    "./ui/html/partials/nav.tmpl",
+    "./ui/html/pages/home.tmpl",
+  }
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal ServerError", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 
 	w.Write([]byte("Hello from Snippetbox"))
@@ -30,8 +51,8 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-    return
+		return
 	}
 
-  w.Write([]byte("Create a new snippet..."))
+	w.Write([]byte("Create a new snippet..."))
 }
