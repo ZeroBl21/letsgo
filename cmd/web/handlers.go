460a95c2ab29a704aeeb14136fbdac687a91776b
Darwin Perdomo <ZeroBl@hotmail.com>
2022-11-21 23:58:07 -0400

    Feat(internals/): Add insert method to SnippetModel

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -46,9 +46,20 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 		app.clientError(w, http.StatusMethodNotAllowed)
+    return
+	}
 
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji, \nBut slowly, slowly!\n\n- Kobayashi Issa"
+	expires := 7
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
 		return
 	}
 
-	w.Write([]byte("Create a new snippet..."))
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+
+	return
 }
