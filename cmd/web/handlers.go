5aa26af7b282cfd5fca5d67d11f90e60a6175cb2
Darwin Perdomo <ZeroBl@hotmail.com>
2022-11-24 15:16:00 -0400

    Feat(web/): Add view page template and templates struct

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,9 +3,9 @@ package main
 import (
 	"errors"
 	"fmt"
+	"html/template"
 	"net/http"
 	"strconv"
-	// "text/template"
 
 	"github.com/zerobl21/letsgo/internal/models"
 )
@@ -16,15 +16,15 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-  snippets, err := app.snippets.Latest()
-  if err  != nil {
-    app.serverError(w, err)
-    return
-  }
-  
-  for _, snippet := range snippets {
-    fmt.Fprintf(w, "%+v\n", snippet)
-  }
+	snippets, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%+v\n", snippet)
+	}
 
 	// files := []string{
 	// 	"./ui/html/base.tmpl",
@@ -62,7 +62,27 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "%+v", snippet)
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/view.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+  data := &templateData{
+    Snippet: snippet,
+  }
+
+	err = ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		app.serverError(w, err)
+	}
+
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
---
+++ cmd/web/templates.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/zerobl21/letsgo/internal/models"
+
+type templateData struct {
+  Snippet *models.Snippet
+}
