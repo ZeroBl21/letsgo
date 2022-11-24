a9868382c3c79f84300e314142c62bb7eb3e9f77
Darwin Perdomo <ZeroBl@hotmail.com>
2022-11-24 13:21:02 -0400

    Feat(web/): Change home handler to print the latest 10 snippets

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"text/template"
+	// "text/template"
 
 	"github.com/zerobl21/letsgo/internal/models"
 )
@@ -16,22 +16,32 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	files := []string{
-		"./ui/html/base.tmpl",
-		"./ui/html/partials/nav.tmpl",
-		"./ui/html/pages/home.tmpl",
-	}
+  snippets, err := app.snippets.Latest()
+  if err  != nil {
+    app.serverError(w, err)
+    return
+  }
+  
+  for _, snippet := range snippets {
+    fmt.Fprintf(w, "%+v\n", snippet)
+  }
 
-	ts, err := template.ParseFiles(files...)
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
-
-	err = ts.ExecuteTemplate(w, "base", nil)
-	if err != nil {
-		app.serverError(w, err)
-	}
+	// files := []string{
+	// 	"./ui/html/base.tmpl",
+	// 	"./ui/html/partials/nav.tmpl",
+	// 	"./ui/html/pages/home.tmpl",
+	// }
+	//
+	// ts, err := template.ParseFiles(files...)
+	// if err != nil {
+	// 	app.serverError(w, err)
+	// 	return
+	// }
+	//
+	// err = ts.ExecuteTemplate(w, "base", nil)
+	// if err != nil {
+	// 	app.serverError(w, err)
+	// }
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
