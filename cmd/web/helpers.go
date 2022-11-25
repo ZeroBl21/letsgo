fb16600f62f1021b42ccd228616632eebb024fb5
Darwin Perdomo <ZeroBl@hotmail.com>
2022-11-25 13:21:53 -0400

    style(web/handlers): Add render method to reduce the code duplicity

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -28,3 +28,21 @@ func (app *application) clientError(w http.ResponseWriter, status int) {
 func (app *application) notFound(w http.ResponseWriter) {
 	app.clientError(w, http.StatusNotFound)
 }
+
+// Renders a page if not render before add to templateCache.
+func (app *application) render(w http.ResponseWriter, status int, page string, data *templateData) {
+	ts, ok := app.templateCache[page]
+	if !ok {
+		err := fmt.Errorf("the template %s does not exist", page)
+		app.serverError(w, err)
+		return
+	}
+
+	w.WriteHeader(status)
+
+	err := ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		app.serverError(w, err)
+	}
+
+}
