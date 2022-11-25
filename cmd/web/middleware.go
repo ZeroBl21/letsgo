81b3f48824f366dc47afcc5b43d57bd43147ef66
Darwin Perdomo <ZeroBl@hotmail.com>
2022-11-25 16:18:39 -0400

    feat(web/): Add recovery to panic functionality middleware to the app

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,7 +1,11 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
+// Add secure headers to every request
 func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set(
@@ -17,9 +21,25 @@ func secureHeaders(next http.Handler) http.Handler {
 	})
 }
 
+// Log the remote addres, proto, method and request URI from every request
 func (app *application) logRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+// Recovers to panic and send http response 500 with Connection Close header
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				w.Header().Set("Connection", "close")
+
+				app.serverError(w, fmt.Errorf("%s", err))
+			}
+		}()
 
 		next.ServeHTTP(w, r)
 	})
