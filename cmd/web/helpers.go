203f80d6562a0c882efb82eb0809141a7dd7e2a8
Darwin Perdomo <ZeroBl@hotmail.com>
2022-11-25 13:42:58 -0400

    perf(web/helpers): Add bytes buffer to check render errors

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"runtime/debug"
@@ -38,11 +39,15 @@ func (app *application) render(w http.ResponseWriter, status int, page string, d
 		return
 	}
 
+  buf := new(bytes.Buffer)
+
+  err :=  ts.ExecuteTemplate(buf, "base", data)
+  if err != nil {
+    app.serverError(w, err)
+    return
+  }
+
 	w.WriteHeader(status)
 
-	err := ts.ExecuteTemplate(w, "base", data)
-	if err != nil {
-		app.serverError(w, err)
-	}
-
+  buf.WriteTo(w)
 }
