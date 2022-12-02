3f000b3ee77f70bbcd45c15f2d0804c45bd3ddd0
Darwin Perdomo <ZeroBl@hotmail.com>
2022-12-01 21:15:55 -0400

    feat(web/): Add Logout functionaity to the login page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -240,6 +240,14 @@ func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
-	// TODO
-	fmt.Fprintln(w, "Logout the user...")
+	err := app.sessionManager.RenewToken(r.Context())
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	app.sessionManager.Remove(r.Context(), "authenticatedUserID")
+	app.sessionManager.Put(r.Context(), "flash", "You've been logged out successful!")
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
