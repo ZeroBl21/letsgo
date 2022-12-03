25ff47543236bb2a7c6f5c79062f4d6e1656ac8e
Darwin Perdomo <ZeroBl@hotmail.com>
2022-12-03 16:02:34 -0400

    test(web/): Add end to end testing to SignupView

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"bytes"
+	"html"
 	"io"
 	"log"
 	"net/http"
 	"net/http/cookiejar"
 	"net/http/httptest"
+	"net/url"
+	"regexp"
 	"testing"
 	"time"
 
@@ -14,6 +17,18 @@ import (
 	"github.com/go-playground/form/v4"
 	"github.com/zerobl21/letsgo/internal/models/mocks"
 )
+
+var csrfTokenRX = regexp.MustCompile(`<input type="hidden" name="csrf_token" value="(.+)">`)
+
+// Extract the CSRF token for the reqeust body
+func extractCSRFToken(t *testing.T, body string) string {
+	matches := csrfTokenRX.FindStringSubmatch(body)
+	if len(matches) < 2 {
+		t.Fatal("no csrf token found in body")
+	}
+
+	return html.UnescapeString(string(matches[1]))
+}
 
 // Returns an instance of the application struct containing
 // mocked dependencies
@@ -73,6 +88,24 @@ func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, strin
 	defer rs.Body.Close()
 
 	body, err := io.ReadAll(rs.Body)
+	bytes.TrimSpace(body)
+
+	return rs.StatusCode, rs.Header, string(body)
+}
+
+// makes a POST request to the test server and returns status code, headers
+// and body
+func (ts *testServer) postForm(t *testing.T, urlPath string, form url.Values) (int, http.Header, string) {
+	rs, err := ts.Client().PostForm(ts.URL+urlPath, form)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer rs.Body.Close()
+
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
 	bytes.TrimSpace(body)
 
 	return rs.StatusCode, rs.Header, string(body)
