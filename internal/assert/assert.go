b04a77432c52ea61a4da58f95ce451c70d30e40d
Darwin Perdomo <ZeroBl@hotmail.com>
2022-12-03 14:43:02 -0400

    test(web/): Add end to end testing to SnippetView

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -1,11 +1,24 @@
 package assert
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
+// Check if the passed two T values are equal.
 func Equal[T comparable](t *testing.T, actual, expected T) {
 	t.Helper()
 
 	if actual != expected {
 		t.Errorf("got: %v; want: %v", actual, expected)
+	}
+}
+
+// Check if a strings contains the expected sub string.
+func StringContains(t *testing.T, actual, expectedSubstring string) {
+	t.Helper()
+
+	if !strings.Contains(actual, expectedSubstring) {
+		t.Errorf("got: %q; expected to contain: %q", actual, expectedSubstring)
 	}
 }
